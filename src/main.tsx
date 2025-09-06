a840c4bae34a91482df77c71f15a67ba923fa02c
tonmoy <tonmoy3673@gmail.com>
2025-09-06 19:00:02 +0600

    :truck: set routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import RootLayout from "./components/layouts/RootLayout";
+import Todos from "./components/pages/Todos";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Login from "./components/pages/Login";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Todos />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
